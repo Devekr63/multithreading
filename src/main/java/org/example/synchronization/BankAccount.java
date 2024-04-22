ff869bb43ddacd1a42b57d4e98cd1fbe4b2fda73
Devendra <devekr12@gmail.com>
2024-04-22 21:56:13 +0530

    Synchronization.

--- src/main/java/org/example/synchronization/BankAccount.java
+++ src/main/java/org/example/synchronization/BankAccount.java
@@ -2,32 +2,64 @@ package org.example.synchronization;
 
 public class BankAccount {
 
-    private double accountBalance;
+    private Double accountBalance;
+    private String name;
 
-    public BankAccount(double accountBalance) {
+    private final Object lockName = new Object();
+    private final Object lockAccountBalance = new Object();
+
+
+    public BankAccount(String name, double accountBalance) {
+        this.name = name;
         this.accountBalance = accountBalance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        synchronized(lockName){
+            this.name = name;
+            System.out.printf("Updated name of the account: %s",name);
+        }
+
     }
 
     public double getAccountBalance() {
         return accountBalance;
     }
 
-    public void deposit(double balance){
+    public  void deposit(double balance){
         try{
+            System.out.println("\nDepositing: adding balance to the account.");
             Thread.sleep(1000);
         }catch (InterruptedException ie){
             throw new RuntimeException(ie);
         }
 
-        double currentBalance = accountBalance;
-        accountBalance += balance;
-        System.out.printf(
-                "%nMoney deposited to the account. Previous: %.0f, added: %.0f, current balance: %.0f%n"
-                , currentBalance, balance, accountBalance
-        );
+        //synchronising all threads that update the currentBalance.
+        //a thread locks the shared variable before execution.
+        synchronized(lockAccountBalance) {
+            double currentBalance = accountBalance;
+            accountBalance += balance;
+            System.out.printf(
+                    "%nMoney deposited to the account. Previous: %.0f, added: %.0f, current balance: %.0f%n"
+                    , currentBalance, balance, accountBalance
+            );
+            promoDolors(100);
+        }
     }
 
-    public void withdraw(double balance){
+    public void promoDolors(double amount){
+        synchronized (lockAccountBalance) {
+            if (accountBalance >= 50000.0) {
+                accountBalance += amount;
+            }
+        }
+    }
+
+    public synchronized void withdraw(double balance){
         try{
             Thread.sleep(1000);
         }catch (InterruptedException ie){
--- src/main/java/org/example/synchronization/Main.java
+++ src/main/java/org/example/synchronization/Main.java
@@ -2,10 +2,10 @@ package org.example.synchronization;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount(100000.0);
+        BankAccount bankAccount = new BankAccount("Devendra",100000.0);
 
         Thread t1 = new Thread(() -> {
-           bankAccount.deposit(20000.0);
+           bankAccount.withdraw(20000.0);
         });
 
         Thread t2 = new Thread(()->{
@@ -13,17 +13,26 @@ public class Main {
         });
 
         Thread t3 = new Thread(() -> {
+            bankAccount.setName("Devendra Kumar");
+        });
+
+        Thread t4 = new Thread(() -> {
             bankAccount.deposit(15000.0);
         });
 
+        //started all the threads
         t1.start();
         t2.start();
         t3.start();
+        t4.start();
 
+        //threads joins main thread
+        //until all tasks by threads(t1,t2,t3) are not completed, till then main thread is waiting.
         try{
             t1.join();
             t2.join();
             t3.join();
+            t4.join();
         }catch (InterruptedException ie){
             ie.printStackTrace();
         }
